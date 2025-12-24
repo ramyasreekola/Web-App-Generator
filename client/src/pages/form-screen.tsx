34129370e12e705b76395d8b8687ebb0135da4d2
Ramya Sree Kola <ramya.sree.kola@volvocars.com>
2025-12-24 16:54:59 +0100

    removed preferred contact method

--- client/src/pages/form-screen.tsx
+++ client/src/pages/form-screen.tsx
@@ -307,40 +307,6 @@ export default function FormScreen() {
                       />
                     </div>
 
-                    {/* Contact Method */}
-                    <div className="space-y-4">
-                      <h3 className="text-lg font-medium border-b pb-2">{t.contactMethod}</h3>
-                      <FormField
-                        control={form.control}
-                        name="extraChoice"
-                        render={({ field }) => (
-                          <FormItem>
-                            <FormControl>
-                              <RadioGroup
-                                onValueChange={field.onChange}
-                                defaultValue={field.value}
-                                className="flex flex-col space-y-1"
-                              >
-                                <FormItem className="flex items-center space-x-3 space-y-0">
-                                  <FormControl>
-                                    <RadioGroupItem value="email" />
-                                  </FormControl>
-                                  <FormLabel className="font-normal">{t.email}</FormLabel>
-                                </FormItem>
-                                <FormItem className="flex items-center space-x-3 space-y-0">
-                                  <FormControl>
-                                    <RadioGroupItem value="phone" />
-                                  </FormControl>
-                                  <FormLabel className="font-normal">{t.phone}</FormLabel>
-                                </FormItem>
-                              </RadioGroup>
-                            </FormControl>
-                            <FormMessage />
-                          </FormItem>
-                        )}
-                      />
-                    </div>
-
                     {/* Consent */}
                     <div className="bg-secondary/20 p-4 rounded-lg space-y-4">
                       <FormField
